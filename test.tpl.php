19da527da4906e99237e2644b2711bd3680350e6
ParlaknFX <ckerallan@gmail.com>
2024-08-03 05:22:47 +0200

    Update test.tpl.php

--- test.tpl.php
+++ test.tpl.php
@@ -1,3 +1,11 @@
+<!-- index.tpl.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+</head>
 <body>
-  test
+    <h1><?php echo $title; ?></h1>
+    <p><?php echo $content; ?></p>
 </body>
+</html>
